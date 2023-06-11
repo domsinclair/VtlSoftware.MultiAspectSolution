9681c960357e5c107cc268fa257e04c54ef36df5
Dom Sinclair <dom@viewtolearn.co.uk>
2023-06-11 16:43:36 +0100

    Added logging aspects built off the back of netstandard 2.1

---
+++ VtlSoftware.Aspects.Logging21/Aspects/RedactAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace VtlSoftware.Aspects.Logging21
+{
+    /// <summary>
+    /// An attribute for redacting parameter or return values. This class cannot be inherited.
+    /// </summary>
+    ///
+    /// <remarks>
+    /// If applied in front of Parameters or the retun value of a method the actual value will be logged as "Redacted".
+    /// </remarks>
+    ///
+    /// <seealso cref="T:Attribute"/>
+
+    [AttributeUsage(AttributeTargets.Parameter | AttributeTargets.ReturnValue)]
+    public sealed class RedactAttribute : Attribute
+    {
+    }
+}
